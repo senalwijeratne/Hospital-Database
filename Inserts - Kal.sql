5550163e4fbdb23bc39d24a9471a51d96c0fcbb0
Mohamed Ikhwan <1khwan.m0@gmail.com>
2017-01-07 21:24:05 +0530

    Insert statements
    Might be slight issues. need to check inserts properly.

--- Inserts - Kal.sql
+++ Inserts - Kal.sql
@@ -7,7 +7,7 @@ INSERT INTO ROOMTYPE (description,roomPrice) VALUES
 ('surgery','12000'),
 ('emergency','2200');
 
-INSERT INTO ROOM(roomType,roomID,bedNo,status,patientID) VALUES
+INSERT INTO ROOM(roomType,roomID,bedNo,status,patientID) VALUES -- patientID not in the create tables
 ('1','1','1','1','1'),
 ('1','1','2','1','2'),
 ('1','1','3','1','3'),
@@ -58,7 +58,7 @@ INSERT INTO SCAN(scanName, scanFee) VALUES
 ('Voiding Cystourethrogram ', 7000);	
 
 
-INSERT INTO MHSCAN(invoiceID, patientID, scanID, scanReport, paymentStatus) VALUES 
+INSERT INTO MH_SCAN(invoiceID, patientID, testID, testReport, paymentStatus) VALUES 
 (1, 1, 1,'Patient found to have broken several fingers while trying to play baseball',1),
 (2, 2, 1,'Patient neck dislocated',1),
 (2, 2, 4,'MRI Scan found cabbage instead of brain',1),
@@ -71,17 +71,94 @@ INSERT INTO MHSCAN(invoiceID, patientID, scanID, scanReport, paymentStatus) VALU
 (9, 9, 11,'Voidlings present. Need to be dispersed as soon as possible.',1);
 
 
+INSERT INTO MH_TEST(invoiceID, patientID, scanID, scanReport, paymentStatus) VALUES /*needs to be rewritten*/
+(1, 1, 1,'Patient found to have broken several fingers while trying to play baseball',1),
+(2, 2, 1,'Patient neck dislocated',1),
+(2, 2, 4,'MRI Scan found cabbage instead of brain',1),
+(3, 3, 2,'Baby Appears to be healthy. Father might be disappointed with the race of the child',1),
+(4, 4, 6,'Anomaly found in the left ventricle of heart. It seems to be beating your name, Sandy.',1),
+(5, 5, 7,'Patient discovered two potatoes inside the machine. We are not really sure what this scan does. Please advise. ',1),
+(6, 6, 10,'Baby appears to have retractable wolverine claws inside forearms.',1),
+(7, 7, 9,'Lungs are busted. Need replacement filters as soon as possible',1),
+(8, 8, 8,'No significant remarks.',1),
+(9, 9, 11,'Voidlings present. Need to be dispersed as soon as possible.',1);
+
+/* http://www.hurleymc.com/services/surgical/types-of-surgery/ */
+INSERT INTO SURGERY(surgeryName, surgeryFee, surgeryDescription) VALUES 
+('Outpatient Surgery', 7000,' same day surgery or ambulatory surgery'),
+('Bariatric Surgery', 1000,''),
+('Breast Augmentation Surgery', 2000,'surgery for reconstructive purposes after breast cancer surgery (for example), to correct a breast defect, to increase breast size, or to achieve a younger, more natural appearance.'),
+('Colon and Rectal Surgery', 3000,' advanced surgical and minimally invasive (laparoscopic) options. '),
+('Endocrine Surgery', 4000,'Endocrine surgery is a surgical sub-speciality focusing on surgery of the endocrine glands, including the thyroid gland, the parathyroid glands, the adrenal glands, glands of the endocrine pancreas, and some neuroendocrine glands.'),
+('General Surgery', 5000,'Surgery can be an effective treatment option for a wide range of diseases and disorders'),
+('Gynecological Surgery', 6000,'Gynecological surgery refers to surgery on the female reproductive system.'),
+('Hand Surgery', 7000,'For certain hand injuries and conditions, a better result can be achieved with arthroscopic surgery, '),
+('Head and Neck Surgery', 6000,'Early identification and treatment of oral, head and neck disorders means a higher rate of positive outcomes'),
+('Neurosurgery', 6000,'Specializing in surgery on the brain and other parts of the nervous system'),
+('Ophthalmological Surgery', 6000,'Eye surgery, also known as ocular surgery, is surgery performed on the eye or its adnexa,'),
+;	
+
+
 INSERT INTO MHSURGERY(invoiceID, surgeryID, patientID, employeeID, RdoctorID, COdoctorID, timeScheduled, 
-	RoomTypeID, roomID, bedID, timeOutOfSurgery, timeInSurgery, preSurgeryNotes, 
+	RoomTypeID, roomID, bedID, timeOutOfSurgery, preSurgeryNotes, 
 	postSurgeryNotes, surgeryReport, paymentStatus) VALUES
-(1,1,1,1 
- 1000),
-('X-ray', 1000),
-('X-ray', 1000),
-('X-ray', 1000),
-('X-ray', 1000),
-('X-ray', 1000),
-('X-ray', 1000),
-('X-ray', 1000),
-('X-ray', 1000),
-('X-ray', 1000),
+(1,1,1,1,1,NULL,
+'20170101 10:00:00 AM',4,1,1,'20170101 11:00:00 AM', 
+'patient requires careful attention to fluctuations in blood pressure.',
+'Surgery progressed without any complications.',
+'Hip replacement surgery successfully completed on the right side.',
+1),
+(1,2,1,1,1,NULL,
+'20170102 8:00:00 AM',4,2,1,'20170102 11:00:00 AM', 
+'patient has successfully fasted for 12 hours prior to surgery',
+'Surgery progressed without any complications.',
+'Removed portion of the stomach.',
+1),
+(2,3,2,2,NULL,1, --doctor is a CO doctor... might run into issues here
+'20170103 10:00:00 AM',4,1,1,'20170103 11:00:00 AM', 
+'Patient requested smaller mammarian glands',
+'Surgery progressed without any complications.',
+'Reduced mammary glands',
+1),
+(3,4,3,1,1,NULL,
+'20170104 10:00:00 AM',4,1,1,'20170104 11:00:00 AM', 
+'Cavities have been expunged of any foreign material before surgery',
+'Surgery progressed without any complications.',
+'Colon surgery completed. Follow up required.',
+1),
+(4,5,4,1,1,NULL,
+'20170106 10:00:00 AM',4,1,1,'20170106 11:00:00 AM', 
+'Immediate surgery recommended',
+'Surgery progressed without any complications.',
+'Endocrine surgery performed in record time.',
+1),
+(5,6,5,1,1,NULL,
+'20170107 10:00:00 AM',4,1,1,'20170107 11:00:00 AM', 
+'Patient requires care in being put under anesthesia',
+'Surgery progressed without any complications.',
+'Knee ligament surgery successfully completed on the left knee.',
+1),
+(6,6,6,1,1,NULL,
+'20170108 10:00:00 AM',4,1,1,'20170108 11:00:00 AM', 
+'patient requires careful attention to diet prior to surgery',
+'Surgery progressed without any complications.',
+'Kidney removed with prejudice.',
+1),
+(7,8,7,1,1,NULL,
+'20170109 10:00:00 AM',4,1,1,'20170109 11:00:00 AM', 
+'Need to re connect nervous system',
+'Surgery progressed without any complications.',
+'Patient can now play the piano like a maestro.',
+1),
+(8,1,8,1,1,NULL,
+'20170110 10:00:00 AM',4,1,1,'20170110 11:00:00 AM', 
+'Pubic hair needs to be removed prior to surgery',
+'Surgery progressed without any complications.',
+'Prostate surgery completed',
+1),
+(9,1,9,1,1,NULL,
+'20170111 10:00:00 AM',4,1,1,'20170111 11:00:00 AM', 
+'Pubic hair needs to be removed prior to surgery',
+'Surgery progressed without any complications.',
+'Vasectomy performed.',
+1);
