a55ea1ff9c9842944f7a23f9113241de569f18fe
Peshala <peshaladayanatha@gmail.com>
2017-01-07 19:54:07 +0530

    Create Insertinto_peshala.sql

---
+++ Insertinto_peshala.sql
@@ -0,0 +1,10 @@
+/* by :  Peshala Rasanga */
+INSERT INTO EMPLOYEETYPE  (employeeTypeID ,employeeID	,departmentID	,firstName ,middleName ,lastName,gender,addressline1,addressline2,addressline3,mobileNO,homeNO,joinDate) VALUES
+(),
+
+INSERT INTO RESIDENT_DOCTOR (RdoctorID,employeeID,specialization,	consultationFee,highestQualification,seniority) VALUES
+()
+INSERT INTO ONCALL_DOCTOR (OCdoctorID,employeeID,specialization,consultationFee,	feePerCall,paymentDate,highestQualification)VALUES
+()
+INSERT INTO EMPLOYEE (employeeTypeID,employeeID,departmentID,firstName,middleName,lastName ,gender,addressline1,addressline2,addressline3,mobileNO,homeNO,joinDate) VALUES 
+()
